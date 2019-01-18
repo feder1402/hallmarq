93d17019dd9e7d2c5ec0c07678a5cd1fdd406932
Federico Raggi <federicoraggi@hotmail.com>
2019-01-17 17:48:57 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import CodeMirror from 'react-codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/darcula.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/edit/matchbrackets'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// const App = () => <CodeMirror value="// Code" mode={{name: "javascript", json: true}} onChange={(code) => {console.log(code)}} options={{lineNumbers: true}} />
+// App.displayName = "App"
+
+const options = {
+  lineNumbers: true,
+  mode: { name: "javascript", json: true },
+  theme: "darcula",
+  autoCloseBrackets: true,
+  matchBrackets: true
 }
 
-export default App;
+class App extends Component {
+
+  state = { code: "{\n}" }
+
+  updateCode = (code) => this.setState({ code })
+
+  submitCode = () => {
+    if (this.props.onSubmit) {
+      this.props.onSubmit(this.state.code)
+    }
+  }
+
+  render() {
+    return (
+      <div class="container-fluid">
+        <nav class="navbar justify-content-between" style={{backgroundColor: "#e3f2fd"}}>
+          <form class="form-inline">
+            <button class="btn btn-outline-primary" onClick={this.submitCode}>Render</button>
+          </form>
+        </nav>
+        <div class="card-group">
+          <div class="card">
+            <div class="card-header text-center">
+              JSON
+              </div>
+            <div class="card-body">
+              <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+            </div>
+            <div class="card-footer text-rigth">
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header text-center">
+              Card
+              </div>
+            <div class="card-body">
+              Another card
+              </div>
+            <div class="card-footer text-rigth">
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// import './styles.css';
+// import 'react-sortable-tree/style.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App onSubmit={(code) => {console.log(code)}}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
